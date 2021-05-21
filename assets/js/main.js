2fbe4bb930f10fe9b09555361ccfc3a758571667
Adam <adamrzymski@gmail.com>
2021-05-21 12:59:14 +0100

    Small refactoring

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-const member = {
+const form = {
   _id: document.getElementById("_id"),
   name: document.getElementById("name"),
   email: document.getElementById("email"),
@@ -12,25 +12,27 @@ const member = {
   },
 };
 
+let members = {};
+
 const parse = (str) => new Function(str + "()");
 
 const createBtn = async () => {
-  await submit("POST", "/api/", member.getFormData(), showMember);
+  await submit("POST", "/api/", form.getFormData(), showMember);
   fetchAllBtn();
 };
 
 const deleteBtn = async () => {
-  await submit("DELETE", "/api/" + member._id.value);
+  await submit("DELETE", "/api/" + form._id.value);
   fetchAllBtn();
 };
 
 const updateBtn = async () => {
-  await submit("PATCH", "/api/" + member._id.value, member.getFormData(), showMember);
+  await submit("PATCH", "/api/" + form._id.value, form.getFormData(), showMember);
   fetchAllBtn();
 };
 
 const readBtn = () => {
-  submit("GET", "/api/" + member.name.value, null, showMember);
+  submit("GET", "/api/" + form.name.value, null, showMember);
 };
 
 const fetchAllBtn = async () => {
@@ -38,24 +40,20 @@ const fetchAllBtn = async () => {
 };
 
 const showMember = (o) => {
-  for (const key in o) if (key != "__v") member[key].value = o[key];
+  for (const key in o) if (key != "__v") form[key].value = o[key];
 };
 
 const selectMember = () => {
   const sl = event.target;
   const idx = sl.selectedIndex;
   const id = sl.options[idx].dataset.id;
-  const members = JSON.parse(sl.dataset.all);
   if (members.length == 1 && event.type == "click") showMember(members[0]);
-  else
-    members.forEach((m) => {
-      if (m._id == id) showMember(m);
-    });
+  else members.forEach((m) => {if (m._id == id) showMember(m)});
 };
 
 const populateSelect = (o) => {
-  const sl = member.select;
-  sl.dataset.all = JSON.stringify(o);
+  const sl = form.select;
+  members = o;
   while (sl.options.length > 0) sl.remove(0);
   o.forEach((e, idx) => {
     const opt = document.createElement("option");
@@ -63,14 +61,15 @@ const populateSelect = (o) => {
     sl.appendChild(opt);
   });
   sl.disabled = sl.options.length === 0 ? true : false;
+  document.getElementById("fetchAllButton").hidden = sl.options.length === 0?false:true
 };
 
 const makeEventListeners = () => {
   [...document.getElementsByTagName("button")].forEach((btn) =>
     btn.addEventListener("click", parse(btn.id.replace("Button", "Btn")))
   );
-  member.select.addEventListener("change", selectMember);
-  member.select.addEventListener("click", selectMember);
+  form.select.addEventListener("change", selectMember);
+  form.select.addEventListener("click", selectMember);
 };
 
 const submit = async (method, url, formData, responseProcessor) => {
@@ -79,7 +78,7 @@ const submit = async (method, url, formData, responseProcessor) => {
     (method === "POST" || method === "PATCH") && formData instanceof FormData
       ? JSON.stringify(Object.fromEntries(formData))
       : null;
-  if (method !== "GET") member.form.reset();
+  if (method !== "GET") form.form.reset();
   const response = await fetch(url, { method, body, headers });
 
   if (response.ok) {
